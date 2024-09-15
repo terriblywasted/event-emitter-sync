7b7b79ee4c82dd714a7e70e43b0f6c24a2153efa
alexMolex <aleksej-krivenko@mail.ru>
2024-09-15 19:07:03 +0300

    feat: adding sync remote and local state

--- emitter-sync-pattern.ts
+++ emitter-sync-pattern.ts
@@ -1,7 +1,7 @@
 /* Check the comments first */
 
 import { EventEmitter } from "./emitter";
-import { EventDelayedRepository } from "./event-repository";
+import { EventDelayedRepository, EventRepositoryError } from "./event-repository";
 import { EventStatistics } from "./event-statistics";
 import { ResultsTester } from "./results-tester";
 import { triggerRandomly } from "./utils";
@@ -59,29 +59,142 @@ function init() {
 */
 
 class EventHandler extends EventStatistics<EventName> {
-  // Feel free to edit this class
-
   repository: EventRepository;
 
   constructor(emitter: EventEmitter<EventName>, repository: EventRepository) {
     super();
     this.repository = repository;
 
-    emitter.subscribe(EventName.EventA, () =>
-      this.repository.saveEventData(EventName.EventA, 1)
-    );
+    EVENT_NAMES.forEach((eventName) => {
+      emitter.subscribe(eventName, () => {
+        const currentLocalValue = this.getStats(eventName) + 1;
+    
+        this.setStats(eventName, currentLocalValue)
+    
+        this.repository.saveEventData(eventName, currentLocalValue)
+      });
+    })
   }
 }
 
-class EventRepository extends EventDelayedRepository<EventName> {
-  // Feel free to edit this class
+class Throttler {
+  private func: (...args: unknown[]) => void;
+  private limit: number;
+  private lastFunc: ReturnType<typeof setTimeout> | null = null;
+  private lastRan: number | null = null;
 
-  async saveEventData(eventName: EventName, _: number) {
-    try {
-      await this.updateEventStatsBy(eventName, 1);
-    } catch (e) {
-      // const _error = e as EventRepositoryError;
-      // console.warn(error);
+  constructor(func: (...args: unknown[]) => void, limit: number) {
+    this.func = func;
+    this.limit = limit;
+  }
+
+  public throttle(...args: unknown[]): void {
+    const context = this;
+
+    if (this.lastRan === null) {
+      this.func.apply(context, args);
+      this.lastRan = Date.now();
+    } else {
+      if (this.lastFunc !== null) {
+        clearTimeout(this.lastFunc);
+      }
+
+      this.lastFunc = setTimeout(() => {
+        if ((Date.now() - (context.lastRan as number)) >= context.limit) {
+          context.func.apply(context, args);
+          context.lastRan = Date.now();
+        }
+      }, this.limit - (Date.now() - (this.lastRan as number)));
+    }
+  }
+}
+
+type EventThread = {
+  previousSyncedValue: number;
+  isUpdating: boolean;
+};
+
+const thredNotFoundError = new Error('Thread not found');
+const REQUEST_REMOTE_UPDATE_THROTTLE = 150;
+
+class EventRepository extends EventDelayedRepository<EventName> {
+  eventUpdatingThrteads = new Map<EventName, EventThread>();
+  requestRemoteUpdateTrhrottled: Throttler
+
+  constructor() {
+    super();
+
+    this.requestRemoteUpdateTrhrottled = new Throttler(this.requestRemoteUpdate, REQUEST_REMOTE_UPDATE_THROTTLE);
+  }
+
+  public saveEventData = async (eventName: EventName, currentLocalValue: number) => {
+    this.requestRemoteUpdateTrhrottled.throttle(eventName, currentLocalValue);  
+  }
+
+  private lockThread = (eventName: EventName) => {
+    const thread = this.eventUpdatingThrteads.get(eventName)
+
+    if(!thread) {
+      throw thredNotFoundError
+    }
+
+    this.eventUpdatingThrteads.set(eventName, {
+      ...thread,
+      isUpdating: true
+    })
+  }
+
+  private unlockThread = (eventName: EventName) => {
+    const thread = this.eventUpdatingThrteads.get(eventName)
+
+    if(!thread) {
+      throw thredNotFoundError
+    }
+
+    this.eventUpdatingThrteads.set(eventName, {
+      ...thread,
+      isUpdating: false
+    })
+  }
+
+  private unlockAndUpdateThread = (eventName: EventName, currentSyncedValue: number) => {
+    this.eventUpdatingThrteads.set(eventName, {
+      previousSyncedValue: currentSyncedValue,
+      isUpdating: false
+    })
+  }
+
+  private requestRemoteUpdate = async (eventName: EventName, currentLocalValue: number) => {
+    if(!this.eventUpdatingThrteads.has(eventName)) {
+      this.eventUpdatingThrteads.set(eventName, {
+        previousSyncedValue: 0,
+        isUpdating: false
+      })
+    } 
+    
+    const { isUpdating, previousSyncedValue } = this.eventUpdatingThrteads.get(eventName) as EventThread
+
+    if (isUpdating) {
+      return;
+    }
+
+    try {     
+      this.lockThread(eventName);
+
+      await this.updateEventStatsBy(eventName, currentLocalValue - previousSyncedValue)
+
+      
+      this.unlockAndUpdateThread(eventName, currentLocalValue)
+    } catch (error) {
+      if(error.message === thredNotFoundError.message){
+        throw error
+      }
+
+      if (error === EventRepositoryError.RESPONSE_FAIL) {
+        this.unlockAndUpdateThread(eventName, currentLocalValue)
+      } else {
+        this.unlockThread(eventName)
+      }
     }
   }
 }
